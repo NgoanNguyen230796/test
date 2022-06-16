fc82eb74157ec6e5f83f15c2efd69060c9c81388
NgoanNguyen230796 <ngoanxitrum2307@gmail.com>
2022-06-16 23:36:42 +0900

    “message”

---
+++ tomatoShop/Controllers/AcerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace tomatoShop.Controllers
+{
+    public class AcerController : Controller
+    {
+        // GET: Acer
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ tomatoShop/Controllers/LapTopController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace tomatoShop.Controllers
+{
+    public class LapTopController : Controller
+    {
+        // GET: Desktop
+        public ActionResult LapTop()
+        {
+            return View();
+        }
+    }
+}
